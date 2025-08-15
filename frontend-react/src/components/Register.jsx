d7e72c8162e65ad1b5678914a3a2be89c07706a0
lperaza <lperaza@enerya.com>
2025-08-15 15:49:17 -0600

    login functionality

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -2,6 +2,7 @@ import {React, useState} from 'react'
 import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
 
 const Register = () => {
     const [username, setUsername] = useState('')
@@ -10,6 +11,8 @@ const Register = () => {
     const [errors, setErrors] = useState({})
     const [success, setSuccess] = useState(false)
     const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
 
     const handleRegistration = async (e) => {
         e.preventDefault();
@@ -27,6 +30,7 @@ const Register = () => {
             console.log('Registration Succesful')
             setErrors({})
             setSuccess(true)
+            navigate('/login')
         }catch(error){
             setErrors(error.response.data)
             console.error('Error', error.response.data)
